2808045b0824dbf23cef3f518600a5eaefd26971
Flavio Percoco <flaper87@gmail.com>
2017-04-21 10:30:19 -0500

    roles data and deploy command

--- doit.sh
+++ doit.sh
@@ -76,9 +76,111 @@ resources:
         playbook: $HOME/openshift-ansible/playbooks/byo/config.yml
 EOF_CAT
 
+cat > $HOME/tripleo-heat-templates/openshift.yaml <<-EOF_CAT
+resource_registry:
+  OS::TripleO::Services::Docker: ../puppet/services/docker.yaml
+EOF_CAT
+
+cat > $HOME/openshift_roles_data.yaml <<-EOF_CAT
+- name: Controller
+  CountDefault: 1
+  tags:
+    - primary
+    - controller
+  ServicesDefault:
+    - OS::TripleO::Services::Docker
+- name: Compute
+  CountDefault: 1
+  HostnameFormatDefault: '%stackname%-novacompute-%index%'
+  disable_upgrade_deployment: True
+  ServicesDefault:
+    - OS::TripleO::Services::Docker
+EOF_CAT
+
 LOCAL_IP=${LOCAL_IP:-`/usr/sbin/ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n'`}
 
 cat > $HOME/run.sh <<-EOF_CAT
-time sudo openstack overcloud deploy 
+#!/bin/bash
+
+set -ux
+
+
+### --start_docs
+## Deploying the overcloud
+## =======================
+
+## Prepare Your Environment
+## ------------------------
+
+## * Source in the undercloud credentials.
+## ::
+
+source /home/stack/stackrc
+
+### --stop_docs
+# Wait until there are hypervisors available.
+while true; do
+    count=$(openstack hypervisor stats show -c count -f value)
+    if [ $count -gt 0 ]; then
+        break
+    fi
+done
+
+### --start_docs
+
+
+## * Deploy the overcloud!
+## ::
+time openstack overcloud deploy \
+    --templates $HOME/tripleo-heat-templates \
+    --libvirt-type qemu \
+    --control-flavor oooq_control \
+    --compute-flavor oooq_compute \
+    --ceph-storage-flavor oooq_ceph \
+    --block-storage-flavor oooq_blockstorage \
+    --swift-storage-flavor oooq_objectstorage \
+    --timeout 90 \
+    -e $HOME/cloud-names.yaml \
+    -e $HOME/tripleo-heat-templates/environments/openshift.yaml \
+    -e $HOME/tripleo-heat-templates/environments/network-isolation.yaml \
+    -e $HOME/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
+    -e $HOME/network-environment.yaml  \
+    -e $HOME/tripleo-heat-templates/environments/low-memory-usage.yaml \
+    -e $HOME/enable-tls.yaml \
+    -e $HOME/tripleo-heat-templates/environments/tls-endpoints-public-ip.yaml \
+    -e $HOME/inject-trust-anchor.yaml \
+    -r $HOME/openshift_roles_data.yaml \
+    --validation-warnings-fatal \
+    --ntp-server pool.ntp.org \
+    ${DEPLOY_ENV_YAML:+-e $DEPLOY_ENV_YAML} "$@" && status_code=0 || status_code=$?
+
+### --stop_docs
+# We don't always get a useful error code from the openstack deploy command,
+# so check `openstack stack list` for a CREATE_COMPLETE status.
+if ! openstack stack list | grep -q 'CREATE_COMPLETE'; then
+        # get the failures list
+    openstack stack failures list overcloud --long > /home/stack/failed_deployment_list.log || true
+
+    # get any puppet related errors
+    for failed in $(openstack stack resource list \
+        --nested-depth 5 overcloud | grep FAILED |
+        grep 'StructuredDeployment ' | cut -d '|' -f3)
+    do
+    echo "heat deployment-show out put for deployment: $failed" >> /home/stack/failed_deployments.log
+    echo "######################################################" >> /home/stack/failed_deployments.log
+    heat deployment-show $failed >> /home/stack/failed_deployments.log
+    echo "######################################################" >> /home/stack/failed_deployments.log
+    echo "puppet standard error for deployment: $failed" >> /home/stack/failed_deployments.log
+    echo "######################################################" >> /home/stack/failed_deployments.log
+    # the sed part removes color codes from the text
+    heat deployment-show $failed |
+        jq -r .output_values.deploy_stderr |
+        sed -r "s:\x1B\[[0-9;]*[mK]::g" >> /home/stack/failed_deployments.log
+    echo "######################################################" >> /home/stack/failed_deployments.log
+    # We need to exit with 1 because of the above || true
+    done
+    exit 1
+fi
+exit $status_code
 EOF_CAT
 chmod 755 $HOME/run.sh
